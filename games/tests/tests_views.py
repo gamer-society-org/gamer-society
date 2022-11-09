9ccb6ef4a88689a55d0dbd9057275ca274146cf2
Pedro Lucas Xavier <pedrolucas.xs@gmail.com>
2022-11-09 10:08:27 -0300

    fix: games tests

--- games/tests/tests_views.py
+++ games/tests/tests_views.py
@@ -5,7 +5,7 @@ from games.models import Game
 from championships.models import Championship
 from teams.models import Team
 from rest_framework.authtoken.models import Token
-
+import ipdb
 
 client = APIClient()
 
@@ -56,52 +56,74 @@ class GamesViewTest(TestCase):
 
         cls.user_staff = User.objects.create_user(**cls.user_staff_data)
         cls.user_staff2 = User.objects.create_user(**cls.user_staff2_data)
-        cls.championship = Championship.objects.create(
-            **cls.championship_data, staff_owner=cls.user_staff
-        )
-        cls.team = Team.objects.create(**cls.team_data)
+        cls.token_staff = Token.objects.create(user=cls.user_staff)
+        cls.credentials = client.credentials(HTTP_AUTHORIZATION=f"Token {cls.token_staff}")
+        cls.championship = client.post(
+                f"/api/championships/register/", data=cls.championship_data
+            )
+        cls.team = client.post(
+                f"/api/championships/register/", data=cls.championship_data
+            )
+        cls.team2 = Team.objects.create(**cls.team_data)
+        cls.team3 = Team.objects.create(**cls.team_data)
+        cls.team4 = Team.objects.create(**cls.team_data)
+        cls.team5 = Team.objects.create(**cls.team_data)
+        cls.team6 = Team.objects.create(**cls.team_data)
+        cls.team7 = Team.objects.create(**cls.team_data)
+        cls.team8 = Team.objects.create(**cls.team_data)
+
+        cls.team_list = [cls.team, cls.team2, cls.team3, cls.team4, cls.team5, cls.team6, cls.team7, cls.team8]
+
         cls.game = Game.objects.get_or_create(
-            **cls.game_data, championship=cls.championship
+            **cls.game_data, championship=cls.championship.data["id"]
         )[0]
 
     def test_edit_game_staff(self):
-        token_staff = Token.objects.create(user=self.user_staff)
-        client.credentials(HTTP_AUTHORIZATION=f"Token {token_staff}")
+        for team in self.team_list:
+            client.credentials(HTTP_AUTHORIZATION=f"Token {self.token_staff}")
+            response = client.patch(
+                f"/api/championships/{self.championship.id}/add-teams/{team.id}/"
+            )
+            self.championship.teams.add(team)
 
-        response = client.patch(
-            f"/api/championships/{self.championship.id}/games/{self.game.id}",
-            {"team_1": {self.team}},
+        client.credentials(HTTP_AUTHORIZATION=f"Token {self.token_staff}")
+        response = client.put(
+            f"/api/games/{self.game.id}/",
+            {"team_1": {self.team}, "team_2": {self.team2}, "initial_date": "2023-01-10"},
         )
 
+        ipdb.set_trace()
         self.assertEqual(200, response.status_code)
 
     def test_edit_game_another_staff(self):
+        
         token_staff2 = Token.objects.create(user=self.user_staff2)
-        client.credentials(HTTP_AUTHORIZATION=f"Token {token_staff2}")
 
-        response = client.patch(
-            f"/api/championships/{self.championship.id}/games/{self.game.id}",
+        client.credentials(HTTP_AUTHORIZATION=f"Token {token_staff2}")
+        response = client.put(
+            f"/api/games/{self.game.id}/",
             {"team_1": {self.team}},
         )
 
         self.assertEqual(403, response.status_code)
 
-    def test_delete_game_staff(self):
-        token_staff = Token.objects.create(user=self.user_staff)
-        client.credentials(HTTP_AUTHORIZATION=f"Token {token_staff}")
+    # Teste comentando pois não deve ser possível deletar um ganme de forma isolada, eles são deletados junto com o championship inteiro.
+    # def test_delete_game_staff(self):
+    #     token_staff = Token.objects.create(user=self.user_staff)
+    #     client.credentials(HTTP_AUTHORIZATION=f"Token {token_staff}")
 
-        response = client.delete(
-            f"/api/championships/{self.championship.id}/games/{self.game.id}"
-        )
+    #     response = client.delete(
+    #         f"/api/championships/{self.championship.id}/games/{self.game.id}"
+    #     )
 
-        self.assertEqual(204, response.status_code)
+    #     self.assertEqual(204, response.status_code)
 
-    def test_delete_game_another_staff(self):
-        token_staff2 = Token.objects.create(user=self.user_staff2)
-        client.credentials(HTTP_AUTHORIZATION=f"Token {token_staff2}")
+    # def test_delete_game_another_staff(self):
+    #     token_staff2 = Token.objects.create(user=self.user_staff2)
+    #     client.credentials(HTTP_AUTHORIZATION=f"Token {token_staff2}")
 
-        response = client.delete(
-            f"/api/championships/{self.championship.id}/games/{self.game.id}"
-        )
+    #     response = client.delete(
+    #         f"/api/championships/{self.championship.id}/games/{self.game.id}"
+    #     )
 
-        self.assertEqual(403, response.status_code)
+    #     self.assertEqual(403, response.status_code)
